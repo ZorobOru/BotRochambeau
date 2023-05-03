f9b577e348d7418ec5ff66b2a8fbce2af63817fc
Daniil Karkozov <dkarkozov@gmail.com>
2023-05-03 12:57:29 +0300

    fine

---
+++ bot.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+from handlers import user_hendlers, other_henlers
+
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    config: Config = load_config()
+
+    bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
+
+    dp.include_router(user_hendlers.router)
+    dp.include_router(other_henlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
